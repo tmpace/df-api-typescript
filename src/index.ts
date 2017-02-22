aca15114fe52b362b07b26dac9d838891d4c0438
tmpace <tmpace11@gmail.com>
2017-02-22 12:01:21 -0600

    Add clustering to node process

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import Server from './service/server';
+/* Dependencies */
+import * as cluster from 'cluster'
+import * as os from 'os'
 
-Server.start();
+import Server from './service/server'
+
+const numCPUs = os.cpus().length
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
+  // Fork Workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  Server.start();
+  console.log(`Worker ${process.pid} started`)
+}
+
